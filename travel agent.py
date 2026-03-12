5076ea6dac756a54e97ee17e259157fc01b4b084
emelyrod18 <emelyrodriguez1810@gmail.com>
2026-03-11 20:31:08 -0400

    search country

--- travel agent.py
+++ travel agent.py
@@ -3,4 +3,31 @@ import json
 
 travel_plans = []
 
-link = "https://restcountries.com/v3.1/name{country}"
+
+
+def search_country():
+    country = input("enter a country name: ")
+
+    link = f"https://restcountries.com/v3.1/name/{country}"
+
+    response = requests.get(link)
+    if response.status_code == 200:
+        data = response.json()[0]
+
+        name = data["name"]["official"]
+        capital = data.get("capital", ["not valid"])
+        region = data["region"]
+        population = data["population"]
+        timezones = data["timezones"]
+#put languages pls                       
+                           
+        print("country information")
+        print("Official Name:", name)
+        print("Capital:", capital)
+        print("Region:", region)
+        print("Population:", population)
+        print("Timezones:", ", ".join(timezones))
+
+    else: print("country not found")
+
+search_country()
